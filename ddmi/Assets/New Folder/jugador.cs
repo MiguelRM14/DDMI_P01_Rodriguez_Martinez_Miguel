2fb8cdfb0e266a86a86282938129514c58b901df
si <>
2025-10-20 17:54:20 +0200

    cosas

--- ddmi/Assets/New Folder/jugador.cs
+++ ddmi/Assets/New Folder/jugador.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
-
+[RequireComponent (typeof(Rigidbody2D))]
 public class jugador : MonoBehaviour
 {
+    int speed=  7;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
+        if (Input.GetKey(KeyCode.D))
         {
-            print ("(negro)");
+            transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime,0,0);
+        }
+        /* if (Input.GetKeyDown(KeyCode.D)) 
+        {
+            rb.linearVelocityX = speed;
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.linearVelocityY = 0;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.linearVelocityX = -speed;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.linearVelocityY = 0;
+        }*/
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag ("tronco"))
+        {
+            speed = 4;
+        }
     }
 }
